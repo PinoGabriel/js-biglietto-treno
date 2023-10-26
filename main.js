026f5a60434fb42db2650714e35ee2281ac6c2f1
PinoGabriel <pinogabriel01@gmail.com>
2023-10-26 16:13:25 +0200

    finished

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 
-const chilometri = parseInt (prompt("Inserisci numero di chilometri da percorrere", "150"));
+const chilometri = parseInt(prompt("Inserisci numero di chilometri da percorrere", "150"));
 const eta = prompt("Inserisci la tua età?", "17")
 
-let prezzo = (chilometri * 0,21)
+let prezzo = (chilometri * 0.21)
 let costo;
 
 
-if (eta <= 17) {
+
+if (eta < 18) {
     costo = prezzo - (20/100 * prezzo);
-    document.getElementById("prezzo").innerHTML = "il prezzo del biglietto è di " + num + "€"
-} else if (eta >= 65) {
-    costo = prezzo - (40/100 * prezzo)
-    document.getElementById("prezzo").innerHTML = "il prezzo del biglietto è di " + num + "€"
-} else costo = prezzo
-document.getElementById("prezzo").innerHTML = "il prezzo del biglietto è di " + num + "€"
+    let trainTicket = costo.toFixed(2);
+    document.getElementById("prezzo").innerHTML = "il prezzo del biglietto è di " + trainTicket + "€"
+} else if (eta > 65) {
+    costo = prezzo - (40/100 * prezzo);
+    let trainTicket = costo.toFixed(2);
+    document.getElementById("prezzo").innerHTML = "il prezzo del biglietto è di " + trainTicket + "€"
+} else costo = prezzo; {
+    let trainTicket = costo.toFixed(2);
+    document.getElementById("prezzo").innerHTML = "il prezzo del biglietto è di " + trainTicket + "€"
+}
