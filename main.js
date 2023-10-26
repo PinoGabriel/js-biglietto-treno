2e37762c0ce16bcd9ca3a1555c1b379852755e43
PinoGabriel <pinogabriel01@gmail.com>
2023-10-26 17:56:01 +0200

    added the latest changes

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 
+const user = prompt("inserisci il tuo nome", "Gabriel");
+document.getElementById("benvenuto").innerHTML = `Ciao ${user}`
+
 const chilometri = parseInt(prompt("Inserisci numero di chilometri da percorrere", "150"));
 const eta = prompt("Inserisci la tua età?", "17")
 
@@ -10,16 +13,12 @@ let costo;
 if (eta < 18) {
     costo = prezzo - (20/100 * prezzo);
     let trainTicket = costo.toFixed(2);
-    document.getElementById("prezzo").innerHTML = "il prezzo del biglietto è di " + trainTicket + "€"
+    document.getElementById("biglietto").innerHTML = "il prezzo del biglietto è di " + trainTicket + "€"
 } else if (eta > 65) {
     costo = prezzo - (40/100 * prezzo);
     let trainTicket = costo.toFixed(2);
-    document.getElementById("prezzo").innerHTML = "il prezzo del biglietto è di " + trainTicket + "€"
+    document.getElementById("biglietto").innerHTML = "il prezzo del biglietto è di " + trainTicket + "€"
 } else (costo = prezzo); {
     let trainTicket = costo.toFixed(2);
-    document.getElementById("prezzo").innerHTML = "il prezzo del biglietto è di " + trainTicket + "€"
-}
-
-if (isNaN) {
-    document.getElementById("prezzo").innerHTML = "errore"
+    document.getElementById("biglietto").innerHTML = "il prezzo del biglietto è di " + trainTicket + "€"
 }
