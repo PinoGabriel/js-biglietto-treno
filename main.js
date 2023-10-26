aa2f1e018be093e99772107118e8f6bdabf2cc26
PinoGabriel <pinogabriel01@gmail.com>
2023-10-26 15:16:59 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+
+const chilometri = parseInt (prompt("Inserisci numero di chilometri da percorrere", "150"));
+const eta = prompt("Inserisci la tua età?", "17")
+
+let prezzo = (chilometri * 0,21)
+let costo;
+
+if (eta <= 17) {
+    costo = (prezzo - 20%);
+    document.getElementById("prezzo").innerHTML = "il prezzo del biglietto è di " + prezzo + "€"
+} else if (eta >= 65) {
+    costo = (prezzo - 40%);
+    document.getElementById("prezzo").innerHTML = "il prezzo del biglietto è di " + costo + "€"
+} else costo = prezzo
+document.getElementById("prezzo").innerHTML = "il prezzo del biglietto è di " + costo + "€"
+
+
+
+
+toFixed(2)
